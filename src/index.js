7335eb4028a67163b2bbdfce2cf0b7cfb10fe347
MinJeong Kim <98nba@naver.com>
2021-01-02 20:59:04 +0900

    Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
+import { createStore } from 'redux';
+
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
-number.innerHTML = count;
+const countStore = createStore(countModifier);
 
-const updateText = () => {
-  number.innerHTML = count;
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
+countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+  countStore.dispatch({ type: "ADD" });
+}
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+  countStore.dispatch({ type: "MINUS" });
+}
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
