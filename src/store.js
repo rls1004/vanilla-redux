996900863ca0aa1fdf46078f5a84290c8c646641
MinJeong Kim <98nba@naver.com>
2021-01-04 00:41:53 +0900

    Redux Toolkit - configureStore
    https://github.com/zalmoxisus/redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,19 +1,8 @@
 import {createStore} from "redux";
-import {createAction, createReducer} from "@reduxjs/toolkit";
+import {configureStore, createAction, createReducer} from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//     switch(action.type) {
-//         case addToDo.type:
-//             return [{text: action.payload, id: Date.now() }, ...state];
-//         case deleteToDo.type:
-//             return state.filter(toDo => toDo.id !== action.payload);
-//         default:
-//             return state;
-//     }
-// }
 
 const reducer = createReducer([/* initial state */], {
     // mutate the state
@@ -25,7 +14,8 @@ const reducer = createReducer([/* initial state */], {
         state.filter(toDo => toDo.id !== action.payload)
 })
 
-const store = createStore(reducer);
+// enable Redux Dev Tools
+const store = configureStore({reducer});
 
 export const actionCreators = {
     addToDo,
